5b14cf13d136ac98f35b7e55a8aef3fea64e2c5a
das-mittel <eltonlin98@yahoo.com>
2020-01-08 15:53:24 -0800

    GUI Implementation #3
    - added text box for player to input username
    - added image icons for selecting each ship (normal, time stopper, and bullet dodger)
    I intend to revisit code later to explore options with button transparency & will design new levels of difficulty buttons. Their preset will do for now but overall isn't visually exciting

--- GUIv.py
+++ GUIv.py
@@ -1,3 +1,4 @@
+import pygame
 from tkinter import *
 from PIL import Image,ImageTk #python -m pip install pillow to cmd
 
@@ -14,31 +15,56 @@ photo1 = ImageTk.PhotoImage(imge1)
 
 lab1 = Label(image = photo1)
 lab1.pack()
+
+fn = StringVar()
 #----
 
 #label1 = Label(window, text = "SPACE INVADERS", fg = "blue", bg = "yellow", font = ("arial", 16, "bold"))
 #label1.pack(fill = BOTH, pady = 2, padx = 2)
 
-label2 = Label(window, text = "Username :", width = 20, font = ("arial", 10, "bold"))
+label2 = Label(window, text = "Username :", width = 15, font = ("arial", 10, "bold"))
 label2.place(x = 80, y = 200)
 
-label3 = Label(window, text = "Select Your Ship", width = 20, font = ("arial", 10, "bold"))
+e1 = Text(window, width=20, height=1, font = ("arial", 10, "bold"))
+x = e1.get("1.0", "end-1c")
+e1.insert("end", "")
+e1.place(x = 240, y = 200)
+
+#entry_1 = Entry(window, textvar = fn)
+#entry_1.place(x = 240, y = 200)
+
+label3 = Label(window, text = "Select Your Ship", width = 15, font = ("arial", 10, "bold"))
 label3.place(x = 80, y = 255)
 
-label4 = Label(window, text = "Select Difficulty", width = 20, font = ("arial", 10, "bold"))
-label4.place(x = 80, y = 325)
+#ship selection
+ship1photo = PhotoImage(file = "C:/Users/elton/OneDrive/Desktop/SpaceInvaders/img/shse1.png")
+ship2photo = PhotoImage(file = "C:/Users/elton/OneDrive/Desktop/SpaceInvaders/img/shse2.png")
+ship3photo = PhotoImage(file = "C:/Users/elton/OneDrive/Desktop/SpaceInvaders/img/shse3.png")
 
+sop1 = Button(window, text = 'No powerups', image = ship1photo)
+sop1.place(x = 94, y = 300)
+
+sop2 = Button(window, text = 'Time stopper', image = ship2photo)
+sop2.place(x = 197.5, y = 300)
+
+sop3 = Button(window, text = 'Bullet dodger', image = ship3photo)
+sop3.place(x = 310, y = 300)
+
+label4 = Label(window, text = "Select Difficulty", width = 15, font = ("arial", 10, "bold"))
+label4.place(x = 80, y = 390)
+
+#buttons for controlling difficulty
 b1 = Button(window, text = "easy", width = 12, bg = 'green', fg = 'papayawhip', command = print)
-b1.place(x = 55, y = 400)
+b1.place(x = 55, y = 445)
 
 b2 = Button(window, text = "medium", width = 12, bg = '#ffcc00', fg = 'white', command = print)
-b2.place(x = 180, y = 400)
+b2.place(x = 180, y = 445)
 
 b3 = Button(window, text = "hard", width = 12, bg = 'red', fg = 'papayawhip', command = print)
-b3.place(x = 310, y = 400)
+b3.place(x = 310, y = 445)
 
 b4 = Button(window, text = "necessito exito", width = 12, bg = 'gray', fg = 'papayawhip', command = exit1)
-b4.place(x = 180, y = 550)
+b4.place(x = 180, y = 547)
 
 window.mainloop()
 
